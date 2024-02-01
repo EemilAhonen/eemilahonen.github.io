887c163f87f9a5c898fe196c1e505c370c8191e9
EemilAhonen <eemilahonenc@gmail.com>
2024-02-01 20:00:00 +0200

    Minify

--- c/c.js
+++ c/c.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
   var scrollValue = window.scrollY;
   var scaleValue = Math.max(1.0, 1.5 - scrollValue / 1000);
-  document.querySelector(".hero-image").style.transform =
+  document.querySelector(".m").style.transform =
     "scale(" + scaleValue + ")";
 });
